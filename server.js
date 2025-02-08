8eca5d7bdf9a63f12f70232fa3a2122b303b433d
huzaifa iqbal <huzaifaiqbal5554@gmail.com>
2025-02-09 01:42:53 +0500

    Phase 1 : Step 5: Setup Express Server

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 import express from 'express'
 const app = express()
 import dotenv from 'dotenv'
+import cors from "cors";
+import flash from 'connect-flash'
+import session   from 'express-session'
+
 
 
 // middlewares
 dotenv.config();
-const PORT = process.env.PORT
-console.log(PORT)
+app.use(session({
+  secret: process.env.SECERET_KEY, // Replace with a secure secret key
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false } // Set to true if using HTTPS
+}));
+app.use(flash());
+const PORT = process.env.PORT || 5000
 
 
 
